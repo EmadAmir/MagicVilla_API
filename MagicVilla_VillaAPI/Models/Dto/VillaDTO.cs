78148d5167b5c8b1937138a16915eb51efa93bf8
Emad <24242.imad@gmail.com>
2023-04-04 00:26:40 +0100

    Module-2-2: added the Delete,patch

--- MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -9,5 +9,9 @@ namespace MagicVilla_VillaAPI.Models.Dto
         [MaxLength(30)]
         public string Name { get; set; }
 
+        public int Sqft { get; set; }
+
+        public int Occupancy { get; set; }
+
     }
 }
